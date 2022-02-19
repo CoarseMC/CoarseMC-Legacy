c4c5897fae8ec91aa5433168a1110b479bd56d0f
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-02-19 19:21:57 +0300

    Update Player.php

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -175,20 +175,6 @@ class Player extends Human implements CommandSender, InventoryHolder, IPlayer
 
 	use PlayerSettingsTrait;
 
-	public const OS_ANDROID = 1;
-	public const OS_IOS = 2;
-	public const OS_OSX = 3;
-	public const OS_FIREOS = 4;
-	public const OS_GEARVR = 5;
-	public const OS_HOLOLENS = 6;
-	public const OS_WIN10 = 7;
-	public const OS_WIN32 = 8;
-	public const OS_DEDICATED = 9;
-	public const OS_TVOS = 10;
-	public const OS_ORBIS = 11;
-	public const OS_NX = 12;
-	public const OS_UNKNOWN = -1;
-
 	public const INVENTORY_CLASSIC = 0;
 	public const INVENTORY_POCKET = 1;
 
